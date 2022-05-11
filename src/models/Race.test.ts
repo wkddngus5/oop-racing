08464e740f42b6904bf387b7d5f083119c6c2e95
Ravi <ravi@inflearn.com>
2022-05-11 19:51:55 +0900

    Race 생성자 전진 시도 횟수 입력

--- src/models/Race.test.ts
+++ src/models/Race.test.ts
@@ -5,10 +5,6 @@ const NEGATIVE_NUMBER = -1;
 const DECIMAL_NUMBER = 0.1;
 
 describe('Race', () => {
-  function createRace(carCount: number, sceneCount: number) {
-    return new Race(carCount, sceneCount)
-  }
-
   it('carCount must be natural number', () => {
     function getRaceMaker(carCount: number) {
       return () => new Race(carCount, NATURAL_NUMBER);
@@ -18,8 +14,14 @@ describe('Race', () => {
     expect(getRaceMaker(NEGATIVE_NUMBER)).toThrowError();
     expect(getRaceMaker(DECIMAL_NUMBER)).toThrowError();
   });
-  
-  // it('second parameter must be a natural number', () => {
 
-  // })
+  it('sceneCount must be natural number', () => {
+    function getRaceMaker(sceneCount: number) {
+      return () => new Race(NATURAL_NUMBER, sceneCount);
+    }
+
+    expect(getRaceMaker(NATURAL_NUMBER)).not.toThrowError();
+    expect(getRaceMaker(NEGATIVE_NUMBER)).toThrowError();
+    expect(getRaceMaker(DECIMAL_NUMBER)).toThrowError();
+  });
 });
--- src/models/Race.ts
+++ src/models/Race.ts
@@ -8,6 +8,12 @@ class Race {
     if (carCount % 1 !== 0) {
       throw new Error('차량 수는 자연수만 가능해요');
     }
+    if (sceneCount < 1) {
+      throw new Error('전진 시도 횟수는 자연수만 가능해요');
+    }
+    if (sceneCount % 1 !== 0) {
+      throw new Error('전진 시도 횟수는 자연수만 가능해요');
+    }
   }
 
   get result() {
